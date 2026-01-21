8e821c49b73596abacfce85e9fb218e393ef9d8f
Noah <noah.westerberg.nw@gmail.com>
2026-01-21 20:06:42 +0100

    Create unit tests using JUnit

---
+++ Saab95Test.java
@@ -0,0 +1,86 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Saab95Test {
+    private Saab95 saab95;
+
+    @BeforeEach
+    void init() {
+        saab95 = new Saab95();
+    }
+
+    @Nested
+    class InitialValueTests {
+        @Test
+        void testNrDoors() {
+            assertEquals(2, saab95.getNrDoors());
+        }
+
+        @Test
+        void testEnginePower() {
+            assertEquals(125, saab95.getEnginePower());
+        }
+
+        @Test
+        void testInitialColor() {
+            assertEquals(Color.red, saab95.getColor());
+        }
+
+        @Test
+        void testInitialSpeed() {
+            assertEquals(0, saab95.getCurrentSpeed());
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({"255, 0, 0", "0, 255, 0", "9, 205, 218"})
+    void testSetColor(int r, int g, int b) {
+        Color color = new Color(r, g, b);
+        saab95.setColor(color);
+        assertEquals(color, saab95.getColor());
+    }
+
+    @Test
+    void testTurbo() {
+        double noTurboFactor = saab95.speedFactor();
+        saab95.setTurboOn();
+        double withTurboFactor = saab95.speedFactor();
+
+        assertTrue(noTurboFactor < withTurboFactor);
+
+        saab95.setTurboOff();
+        assertEquals(noTurboFactor, saab95.speedFactor());
+
+        saab95.setTurboOn();
+        assertEquals(withTurboFactor, saab95.speedFactor());
+    }
+
+    @Test
+    void testEngine() {
+        saab95.startEngine();
+        assertEquals(0.1, saab95.getCurrentSpeed());
+        saab95.gas(10);
+        assertNotEquals(0, saab95.getCurrentSpeed());
+        saab95.stopEngine();
+        assertEquals(0, saab95.getCurrentSpeed());
+    }
+
+    @Test
+    void testSpeed() {
+        saab95.gas(100);
+        assertEquals(saab95.getEnginePower(), saab95.getCurrentSpeed());
+        
+        saab95.brake(50);
+        assertEquals(saab95.getEnginePower() / 2, saab95.getCurrentSpeed());
+
+        saab95.brake(50);
+        assertEquals(0, saab95.getCurrentSpeed());
+    }
+}
---
+++ Volvo240Test.java
@@ -0,0 +1,74 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Volvo240Test {
+    Volvo240 volvo240;
+
+    @BeforeEach
+    void init() {
+        volvo240 = new Volvo240();
+    }
+
+    @Nested
+    class InitialValueTests {
+        @Test
+        void testNrDoors() {
+            assertEquals(4, volvo240.getNrDoors());
+        }
+
+        @Test
+        void testEnginePower() {
+            assertEquals(100, volvo240.getEnginePower());
+        }
+
+        @Test
+        void testInitialColor() {
+            assertEquals(Color.black, volvo240.getColor());
+        }
+
+        @Test
+        void testInitialSpeed() {
+            assertEquals(0, volvo240.getCurrentSpeed());
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({"255, 0, 0", "0, 255, 0", "9, 205, 218"})
+    void testSetColor(int r, int g, int b) {
+        Color color = new Color(r, g, b);
+        volvo240.setColor(color);
+        assertEquals(color, volvo240.getColor());
+    }
+
+    @Test
+    void testEngine() {
+        volvo240.startEngine();
+        assertEquals(0.1, volvo240.getCurrentSpeed());
+        volvo240.gas(10);
+        assertNotEquals(0, volvo240.getCurrentSpeed());
+        volvo240.stopEngine();
+        assertEquals(0, volvo240.getCurrentSpeed());
+    }
+
+    @Test
+    void testSpeed() {
+        volvo240.gas(1);
+        assertEquals(1.25, volvo240.getCurrentSpeed());
+
+        volvo240.gas(9001);
+        assertEquals(volvo240.getEnginePower(), volvo240.getCurrentSpeed());
+
+        volvo240.brake(20);
+        assertEquals(volvo240.getEnginePower() - 25, volvo240.getCurrentSpeed());
+
+        volvo240.brake(9001);
+        assertEquals(0, volvo240.getCurrentSpeed());
+    }
+}
